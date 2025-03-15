bfcd170b5f8ae4b01c54c5b7fe50db45192dc1c6
wongcolin45 <wongcolin45@gmail.com>
2025-03-15 14:31:57 -0400

    done

--- SpringBootAPI/src/main/java/com/chess/ChessApiApplication.java
+++ SpringBootAPI/src/main/java/com/chess/ChessApiApplication.java
@@ -3,7 +3,7 @@ package com.chess;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+// java -jar target/demo-0.0.1-SNAPSHOT.jar
 @SpringBootApplication
 public class ChessApiApplication {
 
